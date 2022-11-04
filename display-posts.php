1bb664146be5695447e71d54336845188354c827
atc <birdhive@gmail.com>
2022-11-04 16:11:41 -0400

    Added native copy of digit_to_word (see also new SDG plugin).

--- display-posts.php
+++ display-posts.php
@@ -117,6 +117,22 @@ function birdhive_att_explode( $string = '' ) {
 	return explode( ',', $string );
 }
 
+function digit_to_word($number){
+    switch($number){
+        case 0:$word = "zero";break;
+        case 1:$word = "one";break;
+        case 2:$word = "two";break;
+        case 3:$word = "three";break;
+        case 4:$word = "four";break;
+        case 5:$word = "five";break;
+        case 6:$word = "six";break;
+        case 7:$word = "seven";break;
+        case 8:$word = "eight";break;
+        case 9:$word = "nine";break;
+    }
+    return $word;
+}
+
 // Hide everything within and including the square brackets
 // e.g. for titles matching the pattern "{Whatever} [xxx]" or "[xxx] {Whatever}"
 if ( !function_exists( 'remove_bracketed_info' ) ) {

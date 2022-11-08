a72c462ebe5aa3cf9b7d53bf32cae43598ae8d19
atc <birdhive@gmail.com>
2022-11-08 16:34:06 -0500

    cont.

--- js/dp.js
+++ js/dp.js
@@ -2,6 +2,29 @@ jQuery(document).ready( function($){
 
 	// see https://developer.wordpress.org/reference/functions/get_the_excerpt/
 	
+	// Toggling
+    $( "p.event-excerpt > span.see-more-text" ).click(function() {
+        
+        /*
+        var id = $(this).attr('id');
+        var item_id = id.substr(14); // e.g. toggle_handle_35381
+        var target_id = "#toggle_target_"+item_id;
+        console.log('item_id: '+item_id+"; target_id: "+target_id);
+        $( target_id ).toggle( "fast", function() {
+            // Animation complete.
+        });
+        */
+        
+    });
+	
+	/*let expandables = $('p.event-excerpt > span.see-more-text');
+	
+	expandables.each( function(){
+	
+	});
+	*/
+	
+	/*
 	const itemSeeMore = document.querySelectorAll(
 		"p.event-excerpt > span.see-more-text"
 	);
@@ -15,5 +38,6 @@ jQuery(document).ready( function($){
 			});
 	  	});
 	}
+	*/
 	
-)};
+});

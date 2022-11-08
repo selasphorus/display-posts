6358ea7bc60cf0f8ac12756157b6c75bbb22b063
atc <birdhive@gmail.com>
2022-11-08 12:41:29 -0500

    Added js for expandable excerpts

---
+++ js/dp.js
@@ -0,0 +1,18 @@
+jQuery(document).ready( function($){
+
+	// see https://developer.wordpress.org/reference/functions/get_the_excerpt/
+	
+	const itemSeeMore = document.querySelectorAll(
+	  "p.event-excerpt> span.see-more-text"
+	);
+	
+	if (itemSeeMore) {
+	  itemSeeMore.forEach((item) => {
+		item.addEventListener("click", () => {
+		  item.classList.toggle("hide");
+		  item.nextElementSibling.classList.toggle("hide");
+		});
+	  });
+	}
+
+}

917a64172e8fdfd7309530cff6100836e77d8ec1
atc <birdhive@gmail.com>
2022-11-08 15:56:24 -0500

    ts

--- js/dp.js
+++ js/dp.js
@@ -1,18 +1,20 @@
-jQuery(document).ready( function($){
-
-	// see https://developer.wordpress.org/reference/functions/get_the_excerpt/
+// see https://developer.wordpress.org/reference/functions/get_the_excerpt/
 	
 	const itemSeeMore = document.querySelectorAll(
-	  "p.event-excerpt > span.see-more-text"
+		"p.event-excerpt > span.see-more-text"
 	);
 	
 	if (itemSeeMore) {
-	  itemSeeMore.forEach((item) => {
-		item.addEventListener("click", () => {
-		  item.classList.toggle("hide");
-		  item.nextElementSibling.classList.toggle("hide");
-		});
-	  });
+		itemSeeMore.forEach((item) => {
+			item.addEventListener("click", () => {
+				item.classList.toggle("hide");
+				item.nextElementSibling.classList.toggle("hide");
+			});
+	  	});
 	}
+
+jQuery(document).ready( function($){
+
+	
 
 }

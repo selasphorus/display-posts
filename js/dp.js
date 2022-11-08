8da5f9772f363551d6e5b8e06237486853b634f7
atc <birdhive@gmail.com>
2022-11-08 15:59:55 -0500

    cont.

--- js/dp.js
+++ js/dp.js
@@ -1,4 +1,6 @@
-// see https://developer.wordpress.org/reference/functions/get_the_excerpt/
+jQuery(document).ready( function($){
+
+	// see https://developer.wordpress.org/reference/functions/get_the_excerpt/
 	
 	const itemSeeMore = document.querySelectorAll(
 		"p.event-excerpt > span.see-more-text"
@@ -11,10 +13,6 @@
 				item.nextElementSibling.classList.toggle("hide");
 			});
 	  	});
-	}
-
-jQuery(document).ready( function($){
-
-	
+	}	
 
 }
